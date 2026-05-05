4b60c670ceccf320a89974ede4249d965aac6e76
Pab.gom <pab.gomezm@duocuc.cl>
2026-05-05 15:00:12 -0400

    Ajustes finales

--- src/main/java/com/ecommerce/orden_service/repository/OrdenRepository.java
+++ src/main/java/com/ecommerce/orden_service/repository/OrdenRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ecommerce.orden_service.model.Orden;
 import java.util.List;
 import java.time.LocalDateTime;
-
-
-
 public interface OrdenRepository extends JpaRepository<Orden, Long>{
 
     List<Orden> findByEstado(String estado);
